1c1adbed90cbc605274599ad2702c71270a5e467
Yasukazu Makino <yasukazu@makyno.jp>
2024-09-21 20:39:28 +0900

    L_TURN,R_TURN,N_TURN

--- main.py
+++ main.py
@@ -3,45 +3,40 @@
 accel is a percentage of power to motor
 
 """
-"""
-
-steer is a ratio(0 to 1)
-
-of motor powers
-
-"""
-def steer2(left: bool):
-    if left:
-        cuteBot.motors(accel, steer * accel)
-        basic.pause(100)
-    else:
-        cuteBot.motors(steer * accel, accel)
-        basic.pause(100)
-tracking = 0
-out = 0
-steer = 0
-accel = 0
-cuteBot.singleheadlights(cuteBot.RGBLights.RGB_R, 255, 0, 0)
+L_TURN = -1
+R_TURN = 1
+N_TURN = 0
 accel = 50
 steer = 0.8
+
+# steer is a ratio(0 to 1)
+# 
+# of motor powers
+def steer2(turn: int):
+    if turn < 0:
+        cuteBot.motors(accel / steer, steer * accel)
+    elif turn > 0:
+        cuteBot.motors(steer * accel, accel / steer)
+    else:
+        cuteBot.motors(accel, accel)
+tracking = 0
+out = 0
+cuteBot.singleheadlights(cuteBot.RGBLights.RGB_R, 255, 0, 0)
 
 def on_forever():
     if out == 0:
         if tracking < 0:
             cuteBot.motors(50, 30)
+        elif tracking > 0:
+            cuteBot.motors(30, 50)
         else:
-            if tracking > 0:
-                cuteBot.motors(30, 50)
-            else:
-                cuteBot.motors(50, 50)
+            cuteBot.motors(50, 50)
+    elif tracking > 0:
+        cuteBot.motors(50, 30)
+    elif tracking < 0:
+        cuteBot.motors(30, 50)
     else:
-        if tracking > 0:
-            cuteBot.motors(50, 30)
-        else:
-            if tracking < 0:
-                cuteBot.motors(30, 50)
-            else:
-                cuteBot.motors(50, 50)
+        cuteBot.motors(50, 50)
 basic.forever(on_forever)
 
 def on_every_interval():
@@ -50,12 +45,14 @@ def on_every_interval():
         tracking = 0
         out = 0
     elif cuteBot.tracking(cuteBot.TrackingState.L_UNLINE_R_LINE):
-        steer2(True)
+        steer2(L_TURN)
         tracking = -1
         out = 0
     elif cuteBot.tracking(cuteBot.TrackingState.L_LINE_R_UNLINE):
+        steer2(R_TURN)
         tracking = 1
         out = 0
-    else: # cuteBot.tracking(cuteBot.TrackingState.L_R_UNLINE):
+    else:
+        # cuteBot.tracking(cuteBot.TrackingState.L_R_UNLINE):
         out = 1
 loops.every_interval(200, on_every_interval)
--- main.ts
+++ main.ts
@@ -1,53 +1,40 @@
-/**
- * accel is a percentage of power to motor
- */
 // steer is a ratio(0 to 1)
 // 
 // of motor powers
-function steer2 (left: boolean) {
-    if (left) {
-        cuteBot.motors(accel, steer * accel)
+function steer2 (turn: number) {
+    if (turn < 0) {
+        cuteBot.motors(accel / steer, steer * accel)
+    } else if (turn > 0) {
+        cuteBot.motors(steer * accel, accel / steer)
     } else {
-        cuteBot.motors(steer * accel, accel)
+        cuteBot.motors(accel, accel)
     }
 }
-let tracking = 0
 let out = 0
+let tracking = 0
 let steer = 0
 let accel = 0
-cuteBot.singleheadlights(cuteBot.RGBLights.RGB_R, 255, 0, 0)
+let N_TURN = 0
+// accel is a percentage of power to motor
+let L_TURN = -1
+let R_TURN = 1
 accel = 50
 steer = 0.8
-basic.forever(function () {
-    if (out == 0) {
-        if (tracking < 0) {
-            cuteBot.motors(50, 30)
-        } else if (tracking > 0) {
-            cuteBot.motors(30, 50)
-        } else {
-            cuteBot.motors(50, 50)
-        }
-    } else if (tracking > 0) {
-        cuteBot.motors(50, 30)
-    } else if (tracking < 0) {
-        cuteBot.motors(30, 50)
-    } else {
-        cuteBot.motors(50, 50)
-    }
-})
+cuteBot.singleheadlights(cuteBot.RGBLights.ALL, 0, 0, 0)
 loops.everyInterval(200, function () {
     if (cuteBot.tracking(cuteBot.TrackingState.L_R_line)) {
         tracking = 0
         out = 0
     } else if (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
-        steer2(true)
+        steer2(L_TURN)
         tracking = -1
         out = 0
     } else if (cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline)) {
-        steer2(false)
+        steer2(R_TURN)
         tracking = 1
         out = 0
     } else {
+        steer2(N_TURN)
         // cuteBot.tracking(cuteBot.TrackingState.L_R_UNLINE):
         out = 1
     }
